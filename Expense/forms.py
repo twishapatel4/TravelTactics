1af53b53e9a5a8a0be18bfb2c8487b5b70bbd706
twishapatel4 <twisha@techeniac.com>
2025-09-04 23:04:08 +0530

    Update forms.py

--- Expense/forms.py
+++ Expense/forms.py
@@ -3,6 +3,12 @@ from .models import Expense
 
 class ExpenseForm(forms.ModelForm):
     class Meta:
+        # model = Expense
+        # fields = ['name', 'amount', 'date']  # Fields to fill in the form
+        # widgets = {
+        #     'date': forms.DateInput(attrs={'type': 'date'}),  # Date input field
+        # }
+## I am editing this code to learn PR
         model = Expense
         fields = ['name', 'amount', 'date']  # Fields to fill in the form
         widgets = {
